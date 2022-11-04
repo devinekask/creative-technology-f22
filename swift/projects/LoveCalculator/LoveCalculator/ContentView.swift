050e111caee2406e1b50c65799991c4b401b0bbb
Wouter Verweirder <wouter.verweirder@gmail.com>
2022-11-04 17:37:56 +0100

    lovecalculator 2022 update

--- swift/projects/LoveCalculator/LoveCalculator/ContentView.swift
+++ swift/projects/LoveCalculator/LoveCalculator/ContentView.swift
@@ -2,71 +2,70 @@
 //  ContentView.swift
 //  LoveCalculator
 //
-//  Created by Wouter Verweirder on 21/09/2021.
+//  Created by Wouter Verweirder on 04/11/2022.
 //
 
 import SwiftUI
 
+func generateUniqueRandomInt(range:ClosedRange<Int>) -> Int {
+    let random = Int.random(in: range)
+    return random
+}
+
+func generateUniqueRandomInt(range:ClosedRange<Int>, except: Int) -> Int {
+    var random:Int
+    repeat {
+        random = generateUniqueRandomInt(range: range)
+    } while (random == except)
+    return random
+}
+
 struct ContentView: View {
-    
     @State var collega1Index:Int
     @State var collega2Index:Int
-    @State var randomPercentage:Int
+    @State var percentage:Int
     
     var body: some View {
-        
         ZStack {
             Image("background_2")
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
-            VStack {
-                Spacer()
+            VStack(spacing: 20) {
+                
                 Image("logo_love_calculator")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.horizontal)
-                Spacer()
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 100)
+                
                 HStack {
                     Image("collega\(collega1Index)")
-                        .resizable(resizingMode: .stretch)
+                        .resizable()
                         .aspectRatio(contentMode: .fit)
                     Image("collega\(collega2Index)")
-                        .resizable(resizingMode: .stretch)
+                        .resizable()
                         .aspectRatio(contentMode: .fit)
                 }
-                .padding(.horizontal)
                 
-                Spacer()
-                Text("\(randomPercentage)% match")
+                Text("\(percentage)% match")
                     .font(.largeTitle)
-                    .foregroundColor(Color.white)
-                Spacer()
+                    .foregroundColor(.white)
                 
                 Button("Shuffle") {
-                    self.collega1Index = Int.random(in: 1...13)
-                    
-                    self.collega2Index = generateUniqueRandomInt(in: 1...13, except: self.collega1Index)
-                    
-                    self.randomPercentage = Int.random(in: 0...100)
+                    collega1Index = generateUniqueRandomInt(range: 1...13)
+                    collega2Index = generateUniqueRandomInt(range: 1...13, except: collega1Index)
+                    percentage = generateUniqueRandomInt(range: 0...100)
                 }
+                .padding()
+                .background(.black)
                 .foregroundColor(.white)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                .font(/*@START_MENU_TOKEN@*/.headline/*@END_MENU_TOKEN@*/)
-                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.black/*@END_MENU_TOKEN@*/)
-                
-                Spacer()
-            }
+                .bold()
+            }.padding()
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        
-        let random1 = generateUniqueRandomInt(in: 1...13, except: 0)
-        let random2 = generateUniqueRandomInt(in: 1...13, except: random1)
-        let randomMatch = Int.random(in: 0...100)
-        
-        ContentView(collega1Index: random1, collega2Index: random2, randomPercentage: randomMatch)
+        ContentView(collega1Index: 1, collega2Index: 2, percentage: 50)
     }
 }
